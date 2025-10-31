9410c13a37c96dda0b94840845c97014fc27f774
SvetiKPa <t7svpa@gmail.com>
2025-10-31 18:37:21 +0100

    HW2

--- users_hw2.py
+++ users_hw2.py
@@ -1,35 +1,49 @@
+# city: строка, минимум 2 символа.
+# street: строка, минимум 3 символа.
+# house_number: число, должно быть положительным.
+# User: Должен содержать следующие поля:
+# name: строка, должна быть только из букв, минимум 2 символа.
+# age: число, должно быть между 0 и 120.
+# email: строка, должна соответствовать формату email.
+# is_employed: булево значение, статус занятости пользователя.
+# address: вложенная модель адреса.
+
 from pydantic import BaseModel, EmailStr, ValidationError, Field, ConfigDict, field_validator
 
 
 class Address(BaseModel):
     model_config = ConfigDict(
-        validate_assignment=True  # Валидация работает даже при изменении полей после создания
+        validate_assignment=True            # Валидация работает даже при изменении полей после создания
     )
-    city: str = Field(min_length=2)   #2
-    street: str = Field(min_length=3) #3
-    house_number: int = Field(gt=0)   #>0
+    city: str = Field(..., min_length=2)    # 2
+    street: str = Field(..., min_length=3)  # 3
+    house_number: int = Field(gt=0)         # >0
 
 
 class User(BaseModel):
     model_config = ConfigDict(
-        str_min_length2=2,
+        str_strip_whitespace=True,
         validate_assignment=True
     )
-    name: str                       #min 2
-    age: int = Field(gt=0, le=120)  #0-120
+    name: str = Field(..., min_length=2)  # min 2
+    age: int = Field(..., gt=0, le=120)  # 0-120
     email: EmailStr
     is_employed: bool = Field(description='если (is_employed = true), его возраст должен быть от 18 до 65 лет.')
     address: Address
 
-    # # def check_age_employed(cls, value: int, ):
-    # #     if value:
-    # @field_validator(mode='after')
-    # def check_age_employment(self):
-    #     if self.is_employed and (self.age < 18 or self.age > 65):
-    #         raise ValueError('Работающий пользователь должен быть в возрасте от 18 до 65 лет')
-    #     return self
+    @field_validator('is_employed', mode="after")
+    @classmethod
+    def check_age_employment(cls, is_employed: bool, info) -> bool:
+#        print(info.data)
+        age = info.data.get('age')
+        # if is_employed is None:
+        #     return age
+        if is_employed and age is not None and (age < 18 or age > 65):
+            raise ValueError('Работающий пользователь должен быть в возрасте от 18 до 65 лет')
+        return is_employed
 
-json_data = ["""{   "name": "John Doe",
+
+json_data = ["""{   "name": "John Doe     ",
     "age": 70,
     "email": "john.doe@example.com",
     "is_employed": true,
@@ -38,23 +52,26 @@ json_data = ["""{   "name": "John Doe",
         "street": "5th Avenue",
         "house_number": 123
     }  }""",
-"""{   "name": " Jo",
-    "age": 13,
-    "email": "john.doe@example.com",
-    "is_employed": true,
-    "address": {
-        "city": "New ",
-        "street": "5th Avenue",
-        "house_number": 1
-    }}"""
+     """{   "name": " Jo",
+         "age": 18,
+         "email": "john.doe@example.com",
+         "is_employed": true,
+         "address": {
+             "city": "New ",
+             "street": "5th Avenue",
+             "house_number": 1
+         }}""",
+     """{   "name": " Joli",
+          "age": 25,
+          "email": "joli.doe@example.com",
+          "is_employed": true,
+          "address": {
+              "city": "New ",
+              "street": "5th Avenue",
+              "house_number": 25
+          }}"""
+
              ]
-
-# try:
-#     user = User.model_validate_json(json_input)
-#     print(f"{user.name}, {user.email}, {user.age}, {user.is_employed}")
-# except ValidationError as e:
-#     print(e)
-
 
 for data in json_data:
     try:
